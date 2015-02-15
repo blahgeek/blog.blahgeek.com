b70edb2f82b1345f01cf4c123b17f493f4fd6c0b
BlahGeek <blahgeek@gmail.com>
2015-02-15 10:32:53 +0800

    image cdn: add link to origin image

--- _plugins/qiniu_cdn_filter.rb
+++ _plugins/qiniu_cdn_filter.rb
@@ -15,7 +15,15 @@ module Jekyll
             cdn_img_suffix = conf['cdn_img_suffix']
             doc = Nokogiri::HTML.fragment(input)
             doc.css("img").each do |img|
-                img.attributes["src"].value = "#{cdn_domain}/#{img.attributes["src"].value}#{cdn_img_suffix}"
+                orig_src = img.attributes["src"].value
+                img.attributes["src"].value = "#{cdn_domain}/#{orig_src}#{cdn_img_suffix}"
+                link = Nokogiri::XML::Node.new "a", doc
+                link["class"] = "qiniu-cdn-img-link"
+                link["href"] = "/#{orig_src}"
+                img.add_previous_sibling(link)
+            end
+            doc.css("a.qiniu-cdn-img-link").each do |link|
+                link.add_child(link.next)
             end
             return doc.to_html
         end
